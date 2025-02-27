ed24a9b8fe0781578567c0a8251c589bc39a857e
[dmurelemn] <89040447+turbo1033@users.noreply.github.com>
2025-02-27 17:13:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from telethon import TelegramClient, events
 import asyncio
 import random
 
-API_ID = '21624015'  # Получи API ID на https://my.telegram.org
-API_HASH = '965bafa712ca522dd5888af4e9679887'  # Получи API HASH на https://my.telegram.org
-PHONE_NUMBER = '+6281213953112'  # Ваш номер телефона в формате +1234567890
+API_ID = ''  # API ID на https://my.telegram.org
+API_HASH = ''  #  API HASH на https://my.telegram.org
+PHONE_NUMBER = ''  # омер телефона в формате +1234567890
 
+# можно добавлять сколько угодно каналов и сколько угодно списков (лучше использовать ID так как закрытые каналы по нику иногда не парсит)
 degen_CHANNELS = [-1001608851478, -1001774085954, -1002415859616, -1001251782083,
               -1001230726405, -1001956398204, -1001965634796, -1001576457836,
               -1002125969105, -1001828950564, -1001866386442, -1001590515612,
@@ -16,9 +17,8 @@ shitpost_channels = [-1002116976604, -1001192195592, -1001700361896,
                      -1001950573084, -1002141098158, -1002005254569, -1002064164863, -1002250659480]
 
 ALL_CHANNELS = degen_CHANNELS + shitpost_channels
-print(ALL_CHANNELS)
 
-# Целевые каналы для пересылки сообщений
+# целевые каналы для пересылки сообщений
 TARGET_CHANNEL_1 = -1002308152299
 TARGET_CHANNEL_2 = -1002419586986
 
@@ -39,7 +39,7 @@ async def handler(event):
         target_chat = TARGET_CHANNEL_2
         message = event.message
 
-    await asyncio.sleep(random.uniform(1, 3))  # Имитируем задержку
+    await asyncio.sleep(random.uniform(1, 3))  # имитируем небольшую задержку
     await client.forward_messages(target_chat, message)
 
 async def main():
